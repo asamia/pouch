4e537aa7a27cbe820b3b6369709c3683fb5afae5
asami.mi <red11451mi@gmail.com>
2021-09-30 05:46:03 +0000

    delete No, add btn instead

--- resources/views/items/index.blade.php
+++ resources/views/items/index.blade.php
@@ -7,19 +7,19 @@
         <table class="table table-striped table-light">
             <thead>
                 <tr>
-                    <th>No</th>
                     <th>アイテム</th>
                     <th>購入日</th>
                     <th>使用期限</th>
+                    <th></th>
                 </tr>
             </thead>
             <tbody>
                 @foreach ($items as $item)
                 <tr>
-                    <td>{!! link_to_route('items.show', $item->id, ['item' => $item->id]) !!}</td>
                     <td>{{ $item->content }}</td>
                     <td>{{ $item->purchase_date }}</td>
                     <td>{{ $item->expiration_date }}</td>
+                    <td>{!! link_to_route('items.show', '詳細', ['item' => $item->id], ['class' => 'btn btn-secondary']) !!}</td>
                 </tr>　
                 @endforeach
             </tbody>
