680805e5cfcce99801e21d945049a5cf2370ee0d
asami.mi <red11451mi@gmail.com>
2021-10-03 07:59:43 +0000

    add footer

--- resources/views/layouts/common.blade.php
+++ resources/views/layouts/common.blade.php
@@ -17,16 +17,23 @@
     </head>
     
     <body>
-        
+        <header>
         {{-- ナビゲーションバー --}}
         @include('layouts.navbar')
+        </header>
         
+        <main>
         <div class="container">
             
              {{-- エラーメッセージ --}}
             @include('error_messages')
             @yield('content')
         </div>
+        </main>
+        
+        <footer>
+            <p>&copy; Mypouch. 2021.</p> 
+        </footer>
         <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/js/bootstrap.bundle.min.js" integrity="sha384-ygbV9kiqUc6oa4msXn9868pTtWMgiQaeYH7/t7LECLbyPA2x65Kgf80OJFdroafW" crossorigin="anonymous"></script>
     </body>
 </html>
