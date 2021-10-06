ae75a17ba583fcf21f575b2495637bfe810b55c5
asami.mi <red11451mi@gmail.com>
2021-10-06 12:17:22 +0000

    add picture

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -5,10 +5,13 @@
         <div class="text-center">
             <h1>My pouch</h1>
             <p>化粧品の使用期限を管理するWebアプリです。</p>
+            
              {{-- ログインページへのリンク --}}
             {!! link_to_route('login', 'ログイン', [], ['class' => 'btn btn-lg btn-primary']) !!}
             {{-- ユーザ登録ページへのリンク --}}
             {!! link_to_route('signup.get', '新規登録', [], ['class' => 'btn btn-lg btn-primary']) !!}
+            
+            <p><img src="{{ asset('images/make2.png')}}"></p>
         </div>
     </div>
 @endsection
